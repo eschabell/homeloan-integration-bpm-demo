f13e68346745bf27951bf73d234c75255efe84e2
mauricedchateau <maurice.de.chateau@gmail.com>
2012-12-14 00:31:38 +0100

    Added check for availability of ESB services, as prerequisite before running the actual tests.

--- projects/homeloan-integration-bpm/src/test/java/org/jbpm/homeloan/ProcessTest.java
+++ projects/homeloan-integration-bpm/src/test/java/org/jbpm/homeloan/ProcessTest.java
@@ -1,5 +1,8 @@
 package org.jbpm.homeloan;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.jbpm.test.JbpmJUnitTestCase;
@@ -13,7 +16,20 @@ public class ProcessTest extends JbpmJUnitTestCase {
     private StatefulKnowledgeSession ksession;
 
     @Before
-    public void setupTestCase() {
+    public void setupTestCase() throws Exception {
+        // Make sure the ESB services are running before starting the tests.
+        boolean found = false;
+        try {
+            found = exists(CreditReportNodeWorkItemHandler.ENDPOINT_ADDRESS + "?wsdl")
+                    && exists(PrequalificationNodeWorkItemHandler.ENDPOINT_ADDRESS + "?wsdl");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+        if (!found) {
+            throw new IllegalStateException("ESB services not found! Please start them before running the tests.");
+        }
+
+        // Set up the knowledge session with the process.
         ksession = createKnowledgeSession("homeloan.bpmn2");
         ksession.getWorkItemManager().registerWorkItemHandler("CreditReportNode", new CreditReportNodeWorkItemHandler());
         ksession.getWorkItemManager().registerWorkItemHandler("PrequalificationNode", new PrequalificationNodeWorkItemHandler());
@@ -28,4 +44,9 @@ public class ProcessTest extends JbpmJUnitTestCase {
         assertNodeTriggered(processInstance.getId(), "Read application", "Credit Report Node", "Prequalification Node", "Communicate approval");
     }
 
+    private static boolean exists(final String wsdlLocation) throws Exception {
+        HttpURLConnection.setFollowRedirects(false);
+        final HttpURLConnection connection = (HttpURLConnection) new URL(wsdlLocation).openConnection();
+        return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+    }
 }
