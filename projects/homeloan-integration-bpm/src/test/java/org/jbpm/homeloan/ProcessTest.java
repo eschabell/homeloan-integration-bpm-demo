305cadc500cfa9066383bdd8ce1f3317fca0ad67
mauricedchateau <maurice.de.chateau@gmail.com>
2012-12-20 10:37:51 +0100

    Added some logging to tests - to see whether the rules are invoked at all.

--- projects/homeloan-integration-bpm/src/test/java/org/jbpm/homeloan/ProcessTest.java
+++ projects/homeloan-integration-bpm/src/test/java/org/jbpm/homeloan/ProcessTest.java
@@ -11,22 +11,28 @@ import javax.xml.bind.JAXB;
 
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
+import org.jbpm.homeloan.creditreport.CreditQuery;
 import org.jbpm.homeloan.prequalification.ApplicationType;
 import org.jbpm.homeloan.prequalification.ObjectFactory;
 import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * This is a sample file to test a process.
  */
 public class ProcessTest extends JbpmJUnitTestCase {
+    private static final String PROCESS_NAME = "HomeLoan.bpmn2";
     private static final String TEST_APPLICATION = "/application.xml";
 
-    private StatefulKnowledgeSession ksession;
+    private static final String SSN_LOW_SCORE = "567-89-1234";
+    private static final String SSN_HIGH_SCORE = "789-12-3456";
 
-    @Before
-    public void setupTestCase() throws Exception {
+    private static StatefulKnowledgeSession ksession;
+
+    @BeforeClass
+    public static void setUpOnce() throws Exception {
         // Make sure the ESB services are running before starting the tests.
         boolean found = false;
         try {
@@ -38,24 +44,37 @@ public class ProcessTest extends JbpmJUnitTestCase {
         if (!found) {
             throw new IllegalStateException("ESB services not found! Please start them before running the tests.");
         }
+    }
 
-        // Set up the knowledge session with the process.
-        ksession = createKnowledgeSession("HomeLoan.bpmn2");
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Set up the knowledge session with the process and handlers.
+        ksession = createKnowledgeSession(PROCESS_NAME);
         ksession.getWorkItemManager().registerWorkItemHandler("CreditReportNode", new CreditReportNodeWorkItemHandler());
         ksession.getWorkItemManager().registerWorkItemHandler("PrequalificationNode", new PrequalificationNodeWorkItemHandler());
     }
 
     @Test
-    public void testProcess() {
-        ApplicationType application = new ObjectFactory().createApplicationType();
-        try {
-            final InputStream is = ProcessTest.class.getResourceAsStream(TEST_APPLICATION);
-            application = JAXB.unmarshal(is, ApplicationType.class);
-        } catch (final DataBindingException dbEx) {
-            dbEx.printStackTrace();
-        }
+    public void happyFlow() {
         final Map<String, Object> parms = new HashMap<String, Object>();
-        parms.put("application", application);
+        parms.put("application", getTestApplication());
+        parms.put("creditQuery", getCreditQuery(SSN_HIGH_SCORE));
+
+        final ProcessInstance processInstance = ksession.startProcess("mortgages.HomeLoan", parms);
+
+        // Check whether the process instance has completed successfully.
+        assertProcessInstanceCompleted(processInstance.getId(), ksession);
+        assertNodeTriggered(processInstance.getId(), "Read application", "Credit Report Node", "Prequalification Node", "Communicate approval");
+    }
+
+    @Test
+    public void lowCreditScore() {
+        final Map<String, Object> parms = new HashMap<String, Object>();
+        parms.put("application", getTestApplication());
+        parms.put("creditQuery", getCreditQuery(SSN_LOW_SCORE));
 
         final ProcessInstance processInstance = ksession.startProcess("mortgages.HomeLoan", parms);
 
@@ -64,9 +83,43 @@ public class ProcessTest extends JbpmJUnitTestCase {
         assertNodeTriggered(processInstance.getId(), "Read application", "Credit Report Node", "Prequalification Node", "Communicate rejection");
     }
 
+    /**
+     * Check whether a given URL hosts a WSDL (or at least some other resource).
+     * 
+     * @param wsdlLocation
+     *            The URL on which the WSDL is supposed to be hosted.
+     * @return Whether the WSDL is hosted at the given URL.
+     * @throws Exception
+     *             If the given URL is not well-formed, no connection could be set up or no response code could be retrieved.
+     */
     private static boolean exists(final String wsdlLocation) throws Exception {
         HttpURLConnection.setFollowRedirects(false);
         final HttpURLConnection connection = (HttpURLConnection) new URL(wsdlLocation).openConnection();
         return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+    }
+
+    /**
+     * Retrieve a loan application for testing purposes, from the configured file location.
+     * 
+     * @return The test application, unmarshalled from XML.
+     */
+    private static ApplicationType getTestApplication() {
+        ApplicationType application = new ObjectFactory().createApplicationType();
+        try {
+            final InputStream is = ProcessTest.class.getResourceAsStream(TEST_APPLICATION);
+            application = JAXB.unmarshal(is, ApplicationType.class);
+        } catch (final DataBindingException dbEx) {
+            dbEx.printStackTrace();
+        }
+        return application;
+    }
+
+    private static CreditQuery getCreditQuery(final String ssn) {
+        final CreditQuery query = new org.jbpm.homeloan.creditreport.ObjectFactory().createCreditQuery();
+        query.setFirstName("John");
+        query.setLastName("Doe");
+        query.setDob("1970/01/01");
+        query.setSsn(ssn);
+        return query;
     }
 }
